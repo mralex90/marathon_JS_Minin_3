d02b95b564313da3f84c83cd1dfd47d31a7aedff
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-24 13:07:08 +0300

    bug fixed

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const upBtn = document.querySelector('.up-button')
 const downBtn = document.querySelector('.down-button')
 const sidebar = document.querySelector('.sidebar')
+const container = document.querySelector('.container')
 const mainSlide = document.querySelector('.main-slide')
 const slidesCount = mainSlide.querySelectorAll('div').length
 
 let activeSlideIndex = 0
 
-sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh` 
 
 upBtn.addEventListener('click', () => {
     changeSlide('up')      
@@ -30,6 +31,7 @@ function changeSlide(direction) {
         }
     }
 
-    mainSlide.style.transform = `translateY
-    (-2000PX)`
+    const height = container.clientHeight
+
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
 }
